a0a9ee44085421d7d271e8156f5046cccc3593cb
AhmedTawfik <ahmed.tawfik.is@gmail.com>
2020-08-11 07:53:43 +0200

    Add more test

--- src/test/java/com/bestseller/starbux/StarbuxApplicationTests.java
+++ src/test/java/com/bestseller/starbux/StarbuxApplicationTests.java
@@ -1,9 +1,7 @@
 package com.bestseller.starbux;
 
 import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 public class StarbuxApplicationTests {
 
     @Test
